37d90858f8310aa216fd8bcbd4b7f1ea0fdb8d57
Sundas Younas <sundas@sundass-mbp.speedport.ip>
2026-09-01 17:25:40 +0200

    Stop tracking Next.js build output

--- .next/dev/server/chunks/ssr/_next-internal_server_app_practice_[mode]_page_actions_06szjc-.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/.next-internal/server/app/practice/[mode]/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=_next-internal_server_app_practice_%5Bmode%5D_page_actions_06szjc-.js.map
